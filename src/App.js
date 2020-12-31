1979f30bb8e638a1f46be9b465cf731d4dc81279
Sebastian Sanabria <sebastian@absulit.com>
2020-12-31 17:01:46 -0600

    base project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import HomePage from './components/homepage/homepage.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      
+    }
+  }
+
+  /*
+  // setState example with state and props in anonymous function
+  handleClick = () => {
+    this.setState((state, props) => {
+      console.log(state, props);
+      return { meaningOfLife: state.meaningOfLife + props.increment }
+    }, () => console.log(this.state.meaningOfLife));
+
+  }
+  */
+
+  render() {
+    return (
+      <div>
+        <HomePage></HomePage>
+      </div>
+    );
+  }
 }
 
 export default App;
