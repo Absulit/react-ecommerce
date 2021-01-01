a50c89ebf425d0af498449c6d88688de8015b988
Sebastian Sanabria <sebastian@absulit.com>
2021-01-01 16:51:06 -0600

    shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import HatsPage from './pages/hatspage/hatspage.component';
+import Shop from './pages/shop/shop.component';
 
 class App extends React.Component {
   constructor(props) {
@@ -29,6 +30,7 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route exact path="/" component={HomePage}/>
+          <Route path="/shop" component={Shop}/>
           <Route path="/shop/hats" component={HatsPage}/>
         </Switch>
       </div>
