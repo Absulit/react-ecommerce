e96b975033664a6f23c2614ca3ac3aba30728045
Sebastian Sanabria <sebastian@absulit.com>
2021-01-01 19:18:35 -0600

    header component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import HatsPage from './pages/hatspage/hatspage.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      
+
     }
   }
 
@@ -28,10 +29,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/shop" component={Shop}/>
-          <Route path="/shop/hats" component={HatsPage}/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={Shop} />
+          <Route path="/shop/hats" component={HatsPage} />
         </Switch>
       </div>
     );
