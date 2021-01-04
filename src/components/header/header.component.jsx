1ccc532fd37a96d5d137a93fc1dc7db129ffd86f
Sebastian Sanabria <sebastian@absulit.com>
2021-01-04 13:41:05 -0600

    fixed header buttons order
    - there were many options (plural one) containing option

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,19 +18,18 @@ const Header = ({ currentUser }) => {
                 <Link className="option" to="/shop">
                     SHOP
                 </Link>
-            </div>
-            <div className="options">
                 <Link className="option" to="/shop">
                     CONTACT
                 </Link>
+                {
+                    currentUser ?
+                        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className="option" to="/signin">SIGN IN</Link>
+                }
+                <CartIcon />
             </div>
-            {
-                currentUser ?
-                    <div className="options" onClick={() => auth.signOut()}>SIGN OUT</div>
-                    :
-                    <Link className="options" to="/signin">SIGN IN</Link>
-            }
-            <CartIcon />
+
         </div>
 
     );
