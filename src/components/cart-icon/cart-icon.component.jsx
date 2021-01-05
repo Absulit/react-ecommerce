eaa92b88bd2b93c63a473fd8468c47def5f6f6f7
Sebastian Sanabria <sebastian@absulit.com>
2021-01-05 14:22:18 -0600

    Memoize via selectors
    - basic variable not needed for memoization because React does it, but useful for further examples

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,6 +8,8 @@ import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
@@ -15,8 +17,10 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
     </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+//const mapStateToProps = ({ cart: { cartItems } }) => ({
+const mapStateToProps = state => ({
+    //itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+    itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
