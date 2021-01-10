a9f2b3c70719c41d2a9fa6fd6328bb4ff73094e1
Sebastian Sanabria <sebastian@absulit.com>
2021-01-09 18:34:34 -0600

    Moving the collections iterator to its own component
    - This to add a different route to list sub content

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-
-import { connect } from "react-redux";
-
-import { selectCollections } from "../../redux/shop/shop.selectors.js";
+import CollectionsOverview from '../../components/colletions-overview/collections-overview.component';
 
 const Shop = ({ collections }) => (
     <div className="shop-page">
-        {
-            collections.map(({ id, ...otherCollectionProps }) => {
-                return <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-            })
-
-        }
+        <CollectionsOverview/>
     </div>
 );
 
-
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(Shop);
+export default Shop;
